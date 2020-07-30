ad39ce2552fb226e305b375075d07cef83569fac
Jason Ahmed <jason_ahmed@yahoo.com>
2020-07-30 12:34:02 -0700

    rewrite get code

--- server.js
+++ server.js
@@ -9,22 +9,27 @@
 
 // "Require" Express.js
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
+
+// Filename paths
+const path = require("path");
+
+// fs is to read and write to files
+const fs = require("fs");
 
 // "Instantiate" server
 const app = express();
 
+// CREATE A ROUTE THAT THE WEBPAGE CAN REQUEST DATA FROM
+const { notes } = require('./notes'); // IS THIS WRONG?
+
 
 // Make server "listen" - IS THIS "DB" THE RIGHT ROUTE?
 app.get('/api/notes', (req, res) => {
+    res.send('Hello');
     res.json(notes);
 });
+
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001`);
 });
-
-
-// CREATE A ROUTE THAT THE WEBPAGE CAN REQUEST DATA FROM
-const { notes } = require('./notes'); // IS THIS WRONG?
